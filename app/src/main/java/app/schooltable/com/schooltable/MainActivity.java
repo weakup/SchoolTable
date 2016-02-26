96cb098c7464f4fe3bbe625eb06481578f509e14
lovecd <916149034@qq.com>
2016-02-26 23:41:16 +0800

    2.26 解析JSON

--- app/src/main/java/app/schooltable/com/schooltable/MainActivity.java
+++ app/src/main/java/app/schooltable/com/schooltable/MainActivity.java
@@ -2,11 +2,9 @@ package app.schooltable.com.schooltable;
 
 import android.app.Activity;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-
+import android.widget.Button;
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -23,6 +21,7 @@ import org.json.JSONTokener;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -30,14 +29,22 @@ import java.util.Objects;
 
 public class MainActivity extends Activity {
 
+    private Button button1_1;
+    private String Demo;
+    private int week=1;
+    private String httpurl="Http://120.27.53.146:5000/api/schedule";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getTable();
+       // button1_1.findViewById(R.id.button1_1);
+
+
     }
     public void  getTable(){
-        String httpurl="Http://120.27.53.146:5000/api/schedule";
+
         RequestQueue mQueue = Volley.newRequestQueue(this);
         StringRequest stringRequest=new StringRequest(Request.Method.POST, httpurl,
                 new Response.Listener<String>() {
@@ -47,34 +54,47 @@ public class MainActivity extends Activity {
                             JSONTokener jsonTokener=new JSONTokener(s);
                             JSONObject Table= (JSONObject) jsonTokener.nextValue();
                             JSONObject schedule=Table.getJSONObject("schedule");
-                            HashMap<String,JSONObject> table_date=new HashMap<>();//每周的课
+                            Map<String,JSONObject> table_date=new LinkedHashMap<>();//每周的课
                             table_date.put("Mon", schedule.getJSONObject("Mon"));
                             table_date.put("Tue", schedule.getJSONObject("Tue"));
                             table_date.put("Wed",schedule.getJSONObject("Wed"));
                             table_date.put("Thu", schedule.getJSONObject("Thu"));
                             table_date.put("Fri", schedule.getJSONObject("Fri"));
                             Iterator D_date=table_date.keySet().iterator();
-                            while (D_date.hasNext()) {
-                                String d_key= (String) D_date.next();
-                                HashMap<String, JSONArray> table_class = new HashMap<>();//每天的课
-                                table_class.put("1-2", table_date.get(d_key).getJSONArray("1-2"));
-                                table_class.put("3-4", table_date.get(d_key).getJSONArray("3-4"));
-                                table_class.put("5-6", table_date.get(d_key).getJSONArray("5-6"));
-                                table_class.put("7-8", table_date.get(d_key).getJSONArray("7-8"));
-                                table_class.put("9-10", table_date.get(d_key).getJSONArray("9-10"));
-                                Iterator C_class = table_class.keySet().iterator();
-                                while (C_class.hasNext()) {//
-                                    String c_key = (String) C_class.next();
-                                    String class_name = table_class.get(c_key).getString(0);
-                                    if (!class_name.equals("null")) {
-                                        JSONTokener json_class = new JSONTokener(class_name);
-                                        JSONObject myclass = (JSONObject) json_class.nextValue();
-                                        Log.d("lsy", "周几:"+d_key+ "第几堂课：" + c_key + "课名:" + myclass.getString("class_name"));
-                                    }
 
-                                }
-                            }
+                                  while (D_date.hasNext()) {
 
+                                    String d_key = (String) D_date.next();
+                                    Map<String, JSONArray> table_class = new LinkedHashMap<>();//每天的课
+                                      table_class.put("1-2", table_date.get(d_key).getJSONArray("1-2"));
+                                      table_class.put("3-4", table_date.get(d_key).getJSONArray("3-4"));
+                                      table_class.put("5-6", table_date.get(d_key).getJSONArray("5-6"));
+                                      table_class.put("7-8", table_date.get(d_key).getJSONArray("7-8"));
+                                      table_class.put("9-10", table_date.get(d_key).getJSONArray("9-10"));
+                                      Iterator C_class = table_class.keySet().iterator();
+                                    while (C_class.hasNext()) {
+                                        String c_key = (String) C_class.next();
+
+                                           String class_name = table_class.get(c_key).getString(0);
+
+                                            if (!class_name.equals("null")) {
+                                                JSONTokener json_class = new JSONTokener(class_name);
+                                                JSONObject myclass = (JSONObject) json_class.nextValue();
+                                                    String wh_week = myclass.getString("weeks");
+                                                    String infos=wh_week;
+                                                    infos=infos.replaceAll("[\\[\\]]", "");
+                                                    String[] info=infos.split(",");
+                                                int[] w=new int[info.length];
+                                                for (int i=0;i<info.length;i++){
+                                                    w[i]=Integer.parseInt(info[i]);
+                                                }
+                                                        Log.d("lsy", "周几=" + d_key + " 第几堂课=" + c_key + " 课名="
+                                                                + myclass.getString("class_name")
+                                                                + " 周数=" +info[0]);
+                                            }
+                                   }
+
+                               }
 
                         } catch (JSONException e) {
                             e.printStackTrace();
